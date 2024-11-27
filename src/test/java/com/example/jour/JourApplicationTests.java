9ac03a2916aae761ecaeb62f9b637572cddab1cc
almost <2137609767@qq.com>
2024-11-28 04:49:44 +0800

    init

---
+++ src/test/java/com/example/jour/JourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
